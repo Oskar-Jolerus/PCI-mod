2f1a013440f6bc8477934be28c69bf0ff4b385c9
CJ Möller <carl.moller@gmx.com>
2017-06-22 15:55:01 +0200

    Add files via upload

--- TurbData.cpp
+++ TurbData.cpp
@@ -7,7 +7,6 @@ TurbData::TurbData()
 
 TurbData::~TurbData()
 {
-	
 }
 
 void TurbData::Open() {
@@ -19,32 +18,32 @@ void TurbData::Open() {
 void TurbData::RawDataToFile() {
 
 	out_file.open("turbData.txt");
-	//int res = m_Xport->StringFromSensor(buffer);
 	int count = 0;
 	int cnum;
-	
-	do {
-		count++;
 
-		if (out_file.is_open()) {
+	if (out_file.is_open()) {
+
+		do {
+
+			count++;
 			int res = m_Xport->StringFromSensor(buffer);
-			
+
 			if ((buffer[7] == '+' || buffer[7] == '-') &&
-				(cnum = sscanf_s(buffer + 7, "%Lf,%Lf,%Lf,%Lf", &yval, &xval, &zval, &Tval)) == 4) {
-				
+				(cnum = sscanf_s(buffer + 7, "%lf,%lf,%lf,%lf", &yval, &xval, &zval, &Tval)) == 4) {
+
 				out_file << "x = " << xval << " y = " << yval << " z = " << zval << " T = " << Tval << endl;
-				//out_file << x.at(count - 1);
 				cout << "hej" << endl;
 			}
 
-		}
-		else {
-
-			cout << "Unable to open file";
-		}
-
-	} while (count < 1500);
+		} while (count < 10);
 	
+	}
+	
+	else {
+
+		cout << "Unable to open file";
+	}
+
 	out_file.close();
 
 }
